b9771715f264fb84a99e0294d57ed459447e28de
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-03-06 20:16:49 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //begin
+    //numeric luas,p,l;
+    double luas,p,l;
+    //display 'masukkan panjangnya ='
+    cout << "masukkan panjangnya = ";
+    //accept p
+    cin >> p;
+    // display 'masukkan lebarnya ='
+    cout << "masukkan lebarnya = ";
+    // accept l
+    cin >> l;
+    // compute luas = p*l
+    luas = p*l;
+    // display "luasnya ="+luas
+    cout << "luasnya = " << luas;
+    // end
+
+}
+
+
